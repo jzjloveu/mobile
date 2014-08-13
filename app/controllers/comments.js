4e474cb727696fc93330ad8242dfbeb50b3063ef
Xiaomeng Wang <chloexiaomengw@gmail.com>
2014-08-13 16:30:03 -0700

    add submit commit

---
+++ app/controllers/comments.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+var CommentsController = Ember.Controller.extend({
+	rate: 0,
+	name: '',
+	note:'',
+	actions: {
+		submit: function() {
+			var currId = this.controllerFor('detail').dishId;
+			var self = this;
+			$.ajax({
+			    //record the data
+			      url: 'http://localhost:3000/comment/add',
+			      data:  {
+			        "userName": self.get('name'),
+			        "dishId": currId,
+			        "content": self.get("note"),
+			        "rate": self.get('rate')
+			      },
+			      type: 'POST',
+			      success: function(data) {
+			      	self.set('name', '');
+			      	self.set('rate', 0);
+			      	self.set('note', '');
+			      	self.transitionTo('list');
+			      }
+	       });
+		}
+	}
+});
+
+export default CommentsController;
--- app/controllers/detail.js
+++ app/controllers/detail.js
@@ -2,13 +2,6 @@ import Ember from 'ember';
 var DetailController = Ember.Controller.extend({
 	queryParams: ['dishId'],
 	dishId:'',
-	showReviews: true,
-	actions:{
-	    toggleReviews: function() {
-	      this.set('showReviews', !this.get('showReviews'));
-	    },
-	},
-
 });
 
 export default DetailController;
